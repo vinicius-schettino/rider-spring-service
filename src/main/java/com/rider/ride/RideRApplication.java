cbe52a2ff98c3b9b7443768745b77e57f977058d
Aluno UniAcademia <alunouni@labcesac.net>
2024-05-08 20:03:08 -0300

    Setup inicial

---
+++ src/main/java/com/rider/ride/RideRApplication.java
@@ -0,0 +1,13 @@
+package com.rider.ride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rider/ride/RideRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rider.ride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
