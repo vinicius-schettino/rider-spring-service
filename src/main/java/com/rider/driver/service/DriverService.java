6d03148ced4878ce37350237d0ef1d2ef17439dd
Wamberg Nascimento <wambergnascimento@gmail.com>
2024-05-09 17:11:17 -0300

    Criação dos metodos para salvar, listar, buscar e deletar Drivers

---
+++ src/main/java/com/rider/driver/service/DriverService.java
@@ -0,0 +1,28 @@
+package com.rider.driver.service;
+
+import com.rider.driver.entities.Driver;
+import com.rider.driver.repositories.DriverRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DriverService {
+    @Autowired
+    private DriverRepository driverRepository;
+
+    public Driver salvar(Driver driver){
+        return driverRepository.save(driver);
+    }
+    public List<Driver> listarDrivers(){
+        return driverRepository.findAll();
+    }
+    public Optional<Driver> bsucarPorId(Integer id){
+        return driverRepository.findById(id);
+    }
+    public void removerDriver(Integer id){
+        driverRepository.deleteById(id);
+    }
+}
