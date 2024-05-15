a58b7f6718f88611688b75a2291edc381fe31335
Wamberg Nascimento <wambergnascimento@gmail.com>
2024-05-14 21:20:29 -0300

    versão mais atual do status

--- src/main/java/com/rider/driver/controller/DriverController.java
+++ src/main/java/com/rider/driver/controller/DriverController.java
@@ -3,31 +3,57 @@ package com.rider.driver.controller;
 import com.rider.driver.entities.Driver;
 import com.rider.driver.entities.DriverStatus;
 import com.rider.driver.service.DriverService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/driver")
+@RequestMapping(value = "/driver", produces = {"application/json"})
+@Tag(name = "driver")
 public class DriverController {
     @Autowired
     private DriverService driverService;
-
-    @PostMapping
+    @Operation(summary = "Realiza o cadastro de novos drivers", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Driver criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
+            @ApiResponse(responseCode = "409", description = "Conflito - o driver já existe"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public Driver salvarDriver(@RequestBody Driver driver) {
         return driverService.salvar(driver);
     }
 
+    @Operation(summary = "Lista todos os drivers existentes no banco de dados", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Drivers listados com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
+            @ApiResponse(responseCode = "404", description = "Nenhum driver encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Driver> listarDrivers() {
         return driverService.listarDrivers();
     }
 
+    @Operation(summary = "Busca um driver pelo seu id", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Driver encontrado"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
+            @ApiResponse(responseCode = "404", description = "Driver não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Driver buscarDriverPorId(@PathVariable("id") Integer id) {
@@ -35,6 +61,13 @@ public class DriverController {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Motorista não encontrado."));
     }
 
+    @Operation(summary = "Deletar um driver usando seu id", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Driver apagado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
+            @ApiResponse(responseCode = "404", description = "Driver não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletarDriverPorId(@PathVariable("id") Integer id) {
@@ -45,7 +78,14 @@ public class DriverController {
                 }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Motorista não encontrado."));
     }
 
-    @PutMapping("/{id}")
+    @Operation(summary = "Atulizar informações do driver pelo seu id", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Driver atualizado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
+            @ApiResponse(responseCode = "404", description = "Driver não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public Driver atualizarDriver(@PathVariable("id") Integer id, @RequestBody Driver driverAtualizado) {
         return driverService.buscarPorId(id)
@@ -57,19 +97,32 @@ public class DriverController {
                 }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Motorista não encontrado."));
     }
 
-    @PostMapping("/start/{id}")
-    // pq não usar um put? - usar id nesse metodo é o correto mesmo? se não, como fazer? - como vamos ter varios caminhos diferentes para mudar para o mesmo estado não seria melhor deixar a maior parte da logica no service?
+    @Operation(summary = "Atualizar o status de um driver de Offline para Available pelo seu id", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Status do driver atualizado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
+            @ApiResponse(responseCode = "404", description = "Driver não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    @PostMapping("/start/{id}") // pq não usar um put? - usar id nesse metodo é o correto mesmo? se não, como fazer? - como vamos ter varios caminhos diferentes para mudar para o mesmo estado não seria melhor deixar a maior parte da logica no service?
     @ResponseStatus(HttpStatus.OK)
     public Driver makeAvailableFromOffline(@PathVariable("id") Integer id) {
         return driverService.buscarPorId(id)
                 .map(driver -> {
                     if (driver.getStatus() == DriverStatus.OFFLINE) {
                         driver.setStatus(DriverStatus.AVAILABLE);
-                    }
+                    } // LEMBRAR DE LANÇAR AS EXCEÇÕES AQUI
                     return driverService.atualizarDriver(driver);
                 }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Motorista não encontrado."));
     }
 
+    @Operation(summary = "Atualizar o status de um driver de Available para Offline pelo seu id", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Status do driver atualizado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
+            @ApiResponse(responseCode = "404", description = "Driver não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @PostMapping("/stop/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Driver makeOfflineFromAvailable(@PathVariable Integer id) {
@@ -82,7 +135,14 @@ public class DriverController {
                 }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Motorista não encontrado."));
     }
 
-    @PostMapping("/accept/{id}") // Eu deveria lançar uma exception caso o usuario tente ir de busy direto para offline e vice versa?
+    @Operation(summary = "Atualizar o status de um driver de Available para Busy pelo seu id", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Status do driver atualizado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
+            @ApiResponse(responseCode = "404", description = "Driver não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    @PostMapping("/accept/{id}") // Eu deveria lançar uma exception caso o usuario tente ir de busy direto para offline e vice versa? 412
     @ResponseStatus(HttpStatus.OK)
     public Driver makeBusyFromAvailable(@PathVariable Integer id) {
         return driverService.buscarPorId(id)
@@ -94,6 +154,13 @@ public class DriverController {
                 }).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Motorista não encontrado."));
     }
 
+    @Operation(summary = "Atualizar o status de um driver de Busy para Available pelo seu id", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Status do driver atualizado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
+            @ApiResponse(responseCode = "404", description = "Driver não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @PostMapping("/finish/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Driver makeAvailableFromBusy(@PathVariable Integer id){
