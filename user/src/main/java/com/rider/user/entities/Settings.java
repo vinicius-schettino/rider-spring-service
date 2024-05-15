20dc9e7767ac4a6bdc7f6ab594a06975dbcba1cc
Marco5678 <juniorpugliese123@gmail.com>
2024-05-15 16:28:22 -0300

    Correção de erro

--- user/src/main/java/com/rider/user/entities/Settings.java
+++ user/src/main/java/com/rider/user/entities/Settings.java
@@ -1,11 +1,15 @@
 package com.rider.user.entities;
 import jakarta.persistence.*;
 
+import java.util.UUID;
 
 @Entity
 @Table
 public class Settings {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id ;
     //language  portugues/ingles/chines/espanhol
     //app_appearance  dark/light
     //notification  on/off
