09045882b1e4c348cdf7843f472cdbb80efced06
Marco5678 <juniorpugliese123@gmail.com>
2024-05-15 21:31:01 -0300

    Adição dos getters e setters da entidade settings

--- user/src/main/java/com/rider/user/entities/Settings.java
+++ user/src/main/java/com/rider/user/entities/Settings.java
@@ -9,10 +9,66 @@ public class Settings {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id ;
-    //language  portugues/ingles/chines/espanhol
-    //app_appearance  dark/light
-    //notification  on/off
-    //ads  on/off
-}
+    private UUID id;
+    private Language language;
+    private AppAppearance appAppearance;
+    private Notification notification;
+    private Ads ads;
 
+    public UUID getId() {
+        return id;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public AppAppearance getAppAppearance() {
+        return appAppearance;
+    }
+
+    public Notification getNotification() {
+        return notification;
+    }
+
+    public Ads getAds() {
+        return ads;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    public void setAppAppearance(AppAppearance appAppearance) {
+        this.appAppearance = appAppearance;
+    }
+
+    public void setNotification(Notification notification) {
+        this.notification = notification;
+    }
+
+    public void setAds(Ads ads) {
+        this.ads = ads;
+    }
+
+    public enum Language {
+        PORTUGUESE, ENGLISH, CHINESE, SPANISH
+    }
+
+    public enum AppAppearance {
+        DARK, LIGHT
+    }
+
+    public enum Notification {
+        ON, OFF
+    }
+
+    public enum Ads {
+        ON, OFF
+    }
+
+}
