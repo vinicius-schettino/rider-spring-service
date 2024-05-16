c961f1ad6338c819a54da42daa86698cbedf12f2
Caio César <ccaio7007@gmail.com>
2024-05-16 16:12:42 -0300

    repository and get/set

---
+++ payment/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO PUBLIC.INVOICE (id, amount, paymentMethod, paymentStatus, invoiceType, userName, driverName, rideDate) VALUES (157, '900,00', null, '2', '0', 'neymarjr', 'NEYMAR JUNIOR', 2023-05-13)
+INSERT INTO PUBLIC.INVOICE (id, amount, paymentMethod, paymentStatus, invoiceType, userName, driverName, rideDate) VALUES (158, '1200,00', null, '2', '0', 'messi10', 'LIONEL MESSI', 2023-04-21);
+INSERT INTO PUBLIC.INVOICE (id, amount, paymentMethod, paymentStatus, invoiceType, userName, driverName, rideDate) VALUES (159, '750,50', null, '1', '1', 'ronaldo7', 'CRISTIANO RONALDO', 2023-06-15);
+INSERT INTO PUBLIC.INVOICE (id, amount, paymentMethod, paymentStatus, invoiceType, userName, driverName, rideDate) VALUES (160, '560,00', null, '3', '0', 'lewandowski9', 'ROBERT LEWANDOWSKI', 2023-07-02);
+INSERT INTO PUBLIC.INVOICE (id, amount, paymentMethod, paymentStatus, invoiceType, userName, driverName, rideDate) VALUES (161, '1300,00', null, '0', '1', 'mbappe7', 'KYLIAN MBAPPE', 2023-08-09);
